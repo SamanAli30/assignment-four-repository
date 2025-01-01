ac2c3872476454c4c0c58c031ad527fcb74b234c
Saman Ali30 <jurneyforpassion@gmail.com>
2025-01-01 10:05:19 +0600

    massage genarate successful

--- assignment-javasceipt.js
+++ assignment-javasceipt.js
@@ -1,15 +1,25 @@
-
 // Problem One Solved
 function calculateTax(income, expenses) {
-  if (income>=0 && expenses>=0 && income > expenses) {
+  if (income >= 0 && expenses >= 0 && income > expenses) {
     const profit = income - expenses;
-    const netTax = (profit *20) /100;
+    const netTax = (profit * 20) / 100;
     return `Net profit:${profit} ,net tax:${netTax}`;
-    
   } else {
     return "Invalid Input";
   }
-
- 
 }
-console.log(calculateTax(2000, 220));
+// Notification Generator
+
+function sendNotification(email) {
+  if (email.indexOf("@") === -1) {
+    return "Invalid Email";
+  } else {
+    const mail = email.split("@");
+    if (mail.length === 2) {
+      let first = mail[0];
+      let second = mail[1];
+      return ` ${first} send you a email form ${second}`;
+    }
+  }
+}
+// console.log(sendNotification("mdsamanali@gmail.com"));
